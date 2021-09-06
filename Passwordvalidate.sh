450dbc10288950b3afa01668dda7b6b19820f7aa
AviramDahan <89352211+AviramDahan@users.noreply.github.com>
2021-09-06 18:40:48 +0200

    script is ready with the return of accurate errors

--- Passwordvalidate.sh
+++ Passwordvalidate.sh
@@ -7,26 +7,41 @@ CLEAR_COLOR='\033[0m'
 
 #PASSWORD 
 echo "Please Enter Your Password please: "
-password='12345678910a'
+password='12345678a'
+CHECK=0
 
+#Returns to the user that the number of letters is less than 10
+if [ ${#password} -gt 9 ];
+then
+((CHECK++))
+else
+echo -e $ERROR_COLOR "Password less than 10 characters "$CLEAR_COLOR
+exit 1
+fi
+
+#Returns to the user that Missing upper letter
+if [[ $password =~ [A-Z] ]];
+then
+((CHECK++))
+else
+echo -e $ERROR_COLOR "password does not include uppercase letters "$CLEAR_COLOR
+exit 1
+fi
+
+
+#Returns to the user that Missing lower letter
+if [[ $password =~ [a-z] ]];
+then
+((CHECK++))
+else
+echo -e $ERROR_COLOR "password does not include lowercase letters "$CLEAR_COLOR
+exit 1
+fi
 
 #Conditions for password Valid len >= 10 && Upper\Lower letters:
-if [[ ${#password} -ge 10 && "$password" == *[A-Z]* && "$password" == *[a-z]* && "$password" == *[0-9]* ]];
+if [ $CHECK -eq 3 ];
 then
    echo -e $VALID_COLOR  $password "
    Password Valid"$CLEAR_COLOR
    exit 0
 fi
-
-#Returns to the user that the number of letters is less than 10
-if [ ${#password} -ge 9 ];
-then
-echo -e $ERROR_COLOR "Password less than 10 characters "$CLEAR_COLOR
-fi
-
-#Returns to the user that Missing upper letter
-
-
-#Returns to the user that Missing lower letter
-
-
